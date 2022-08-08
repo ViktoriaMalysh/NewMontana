a23400e335d632f1c4026032b607718183a4adef
ViktoriaMalysh <vikyskamalyshzp@gmail.com>
2022-08-08 11:22:57 +0300

    header done

--- src/Common/Header/Header.jsx
+++ src/Common/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Button, Menu } from "semantic-ui-react";
+import { Button, Icon, Menu } from "semantic-ui-react";
 import { itemsHeader } from "../../Backend/Data";
 import logo from "../../assets/logo-login.png";
 import styles from "./Header.module.scss";
@@ -11,16 +11,21 @@ const Header = () => {
 			<Menu.Item name="logout" className={styles.headerIcon}>
 				<img alt="logo" src={logo} />
 			</Menu.Item>
-			{itemsHeader.map((item) => (
-				<Menu.Item
-					className={styles.headerItems}
-					name={item.item}
-					// active={activeItem === 'home'}
-					// onClick={this.handleItemClick}
-				/>
-			))}
 
-			<Button type="submit">Book now</Button>
+			<Menu.Menu position="right">
+				{itemsHeader.map((item) => (
+					<Menu.Item
+						className={styles.headerItems}
+						name={item.item}
+						// active={activeItem === 'home'}
+						// onClick={this.handleItemClick}
+					/>
+				))}
+				<Button type="submit">
+					Book now
+					<Icon name="arrow right" className={styles.headerIcon} />
+				</Button>
+			</Menu.Menu>
 		</Menu>
 	);
 };
