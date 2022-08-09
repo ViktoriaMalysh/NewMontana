7adc79921044f0886e7ee3998acbd29ec12f6051
ViktoriaMalysh <vikyskamalyshzp@gmail.com>
2022-08-09 18:49:57 +0300

    create calendar

--- src/Common/Search Area/SearchArea.jsx
+++ src/Common/Search Area/SearchArea.jsx
@@ -1,44 +1,118 @@
-import { Segment, Form, Icon } from "semantic-ui-react";
+import { Segment, Form, Icon, Input, Select, Button } from "semantic-ui-react";
 import styles from "./SearchArea.module.scss";
-import DatePicker from "react-datepicker";
 import { useState } from "react";
-import "react-datepicker/dist/react-datepicker.css";
+import "react-calendar/dist/Calendar.css";
+import Calendar from "react-calendar";
+import "./Calendar.scss";
 
 const SearchArea = () => {
 	const [startDate, setStartDate] = useState(new Date());
+	const [openCalendar, setOpenCalendar] = useState([
+		{ key: "check-in", open: false },
+		{ key: "check-out", open: false },
+	]);
+
+	const handleClose = (key) => {
+		setOpenCalendar(
+			openCalendar.map((item) => {
+				if (item.key === key) item.open = true;
+				return item;
+			})
+		);
+	};
+
+	const formatShortWeekday = (locale, date) =>
+		["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"][date.getDay()];
 
 	return (
 		<Segment className={styles.searchAreaSegment}>
 			<Form>
-				<Form.Group widths="equal">
-					<Form.Input
-						className={styles.searchAreaInput}
-						fluid
-						label="Destination"
-						icon="map marker alternate"
-						iconPosition="left"
-						placeholder="New York, USA"
-					/>
-					<DatePicker
-						className={styles.datePicker}
-						selected={startDate}
-						onChange={(date) => setStartDate(date)}
-					/>
-					<DatePicker
-						selected={startDate}
-						onChange={(date) => setStartDate(date)}
-					/>
+				<Form.Group widths="equal" className={styles.searchAreaGroup}>
+					<Form.Field>
+						<label>Destination</label>
+						<div>
+							<Icon
+								name="map marker alternate"
+								className={styles.searchAreaIcon}
+							/>
+							<input
+								placeholder="New York, USA"
+								type="text"
+								className={styles.searchAreaInput}
+							/>
+						</div>
+					</Form.Field>
+					<Form.Field>
+						<div>
+							<label>Check In</label>
+							{openCalendar && (
+								<div className={styles.searchAreaCalendar}>
+									<Calendar
+										formatShortWeekday={formatShortWeekday}
+										next2Label={<Icon name="angle double right" />}
+										prev2Label={<Icon name="angle double left" />}
+										calendarType="US"
+										name="check-in"
+										showNeighboringMonth={false}
+										// locale="uk"
+										onClickDay={(value, event) => {
+											handleClose('check-in');
+										}}
+									/>
+								</div>
+							)}
 
-					<Form.Select
-						fluid
-						label="Travel Type"
-						// icon="globe"
-						// iconPosition="left"
-						// options={options}
-						placeholder="Travel Type"
-					>
-						<Icon name="globe" />
-					</Form.Select>
+							<input
+								onClick={() => setOpenCalendar(true)}
+								className={styles.searchAreaInputCalendar}
+							/>
+						</div>
+					</Form.Field>
+					<Form.Field>
+						<div>
+							<label>Check Out</label>
+							{openCalendar && (
+								<div className={styles.searchAreaCalendar}>
+									<Calendar
+										formatShortWeekday={formatShortWeekday}
+										next2Label={<Icon name="angle double right" />}
+										prev2Label={<Icon name="angle double left" />}
+										calendarType="US"
+										// name="check-out"
+										showNeighboringMonth={false}
+										// locale="uk"
+										onClickDay={(value, event) => {
+											handleClose(event.target.name);
+										}}
+									/>
+								</div>
+							)}
+
+							<input
+								onClick={() => setOpenCalendar(true)}
+								className={styles.searchAreaInputCalendar}
+							/>
+						</div>
+					</Form.Field>
+					<Form.Field>
+						<Form.Select
+							fluid
+							label="Travel Type"
+							// icon="globe"
+							// iconPosition="left"
+							// options={options}
+							placeholder="Travel Type"
+						>
+							<Icon name="globe" />
+						</Form.Select>
+					</Form.Field>
+
+					<Form.Field>
+						<Button className={styles.searchAreaButton}>
+							<Icon name="search" />
+							Find now
+						</Button>
+					</Form.Field>
 				</Form.Group>
 			</Form>
 		</Segment>
