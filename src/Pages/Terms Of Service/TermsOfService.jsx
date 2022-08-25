525528650612c8c4734671c72fa1b3f0d2b640ae
ViktoriaMalysh <vikyskamalyshzp@gmail.com>
2022-08-25 14:58:01 +0300

    TermsOfService Page done

--- src/Pages/Terms Of Service/TermsOfService.jsx
+++ src/Pages/Terms Of Service/TermsOfService.jsx
@@ -1,17 +1,23 @@
 import { Container, Header } from "semantic-ui-react";
 import { termsOfService } from "../../Backend/Data";
+import Breadcrumb from "../../Common/Breadcrumb/Breadcrumb";
+import Footer from "../../Common/Footer/Footer";
 import styles from "./TermsOfService.module.scss";
 
 const TermsOfService = () => {
 	return (
-		<div className={styles.termsBlock}>
-			{termsOfService.map((item) => (
-				<Container fluid className={styles.termsContainer}>
-					<Header as="h3">{item.title}</Header>
-					<p>{item.text}</p>
-				</Container>
-			))}
-		</div>
+		<>
+			<Breadcrumb title="Terms Of Service" link="terms Of Service" />
+			<div className={styles.termsBlock}>
+				{termsOfService.map((item) => (
+					<Container fluid className={styles.termsContainer}>
+						<Header as="h3">{item.title}</Header>
+						{item.text}
+					</Container>
+				))}
+			</div>
+			<Footer />
+		</>
 	);
 };
 
